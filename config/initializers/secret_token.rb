33770feed3ce623de8783fcbff49775076c68ff3
Sam Lightbody <slight@slight.aida-dom.handysoft.com>
2012-03-29 10:45:56 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Collabnote::Application.config.secret_token = '51f25ff7dccca1def71438faacd1770a844417cabf878627d3d0962f0aacedb2b962e13a582f118139d3fa9df05416180c333a6847de99389e1e553a182898ec'
